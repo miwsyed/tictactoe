cb543898973f8e882bcb748c8554ddeddbb5f9ee
miwsyed <syed97mustafa@gmail.com>
2021-01-16 03:56:15 +0530

    highlight winning squares and added Start NEW Game button

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -1,28 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const StatusMessage = ({ winner, current }) => {
-
-
-    // that we will pass inside and this call back function must return boolean value and for every
-    // element the call back returns true then the whole condition will be set to true AND if atleast 
-    //one element returns false from this callback functoin then the whole condition will be set to false }
-
     const noMovesLeft = current.board.every(el => el !== null);
 
-    return <h2>
-        {
-            winner && `Winner is ${winner}`
-        }
-        {
-            !winner && !noMovesLeft && `Next player is ${current.isXNext ? 'X' : 'O'} `
-        }
-        {
-            !winner && noMovesLeft && "It's a tie"
-        }
-
-    </h2>;
-
-
+    return (
+        <h2>
+            {winner && `Winner is ${winner}`}
+            {!winner &&
+                !noMovesLeft &&
+                `Next player is ${current.isXNext ? 'X' : 'O'}`}
+            {!winner && noMovesLeft && 'X and O tied'}
+        </h2>
+    );
 };
 
 export default StatusMessage;
